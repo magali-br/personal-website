1405add9a85a97e373d8fad17f7a1415fb73780e
Magali BR <magali.bzr@gmail.com>
2024-12-14 05:29:09 -0500

    Add fiction books

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -4,19 +4,34 @@ function Books() {
     return (
         <div className="Books Container">
             <div>
-                <p>Here is an evergrowing list of my favourite nonfiction books (->{" "}
+                <p>Here is an evergrowing list of  {" "}
                     <a
                         className="BoldUnderlined Link"
                         href="https://docs.google.com/spreadsheets/d/11qa-sXCDfMki8rj4OEC3aWGPZcHtS7Ka9keVDM9krj0/view"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        link to the spreadsheet
+                        my favourite nonfiction books
                     </a>
-                    ).</p>
+                    .</p>
             </div>
             <div className="GoogleDoc">
                 <iframe src="https://docs.google.com/spreadsheets/d/11qa-sXCDfMki8rj4OEC3aWGPZcHtS7Ka9keVDM9krj0/view?embedded=true" className="GoogleDoc" width="100%" height="718" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
+            <div>
+                <p>Here is an evergrowing list of {" "}
+                    <a
+                        className="BoldUnderlined Link"
+                        href="https://docs.google.com/spreadsheets/d/13D3mh1jt1iYxR1FC1Dbi0fdxguFoRBHTGjRi1_vPe7U/view"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        my favourite novels
+                    </a>
+                    .</p>
+            </div>
+            <div className="GoogleDoc">
+                <iframe src="https://docs.google.com/spreadsheets/d/13D3mh1jt1iYxR1FC1Dbi0fdxguFoRBHTGjRi1_vPe7U/view?embedded=true" className="GoogleDoc" width="100%" height="718" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
         </div >
     );
