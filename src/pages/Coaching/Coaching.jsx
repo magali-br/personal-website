8c3e283881f0e76872141b92dc32a334fda57637
Magali BR <magali.bzr@gmail.com>
2025-05-01 07:29:37 +0200

    Adding coaching tools section

---
+++ src/pages/Coaching/Coaching.jsx
@@ -0,0 +1,18 @@
+import "./Coaching.css";
+
+function Coaching() {
+    return (
+        <div className="Coaching Container">
+            <h2 className="Subtitle">
+                Values Generator
+            </h2>
+            <p>
+                If you're looking for inspiration for what you could be focussing on in your life right now, values are a great tool.
+            </p>
+            <p>Generate a value and see if it feels aligned. If it doesn't, just generate another. Stay with this value for the day or for the week, and see how it feels and what comes up.
+            </p>
+        </div>
+    );
+}
+
+export default Coaching;
