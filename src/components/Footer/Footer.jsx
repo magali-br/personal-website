433781ba2291ea97d4bf0bba2261b9a195270640
Magali BR <magali.bzr@gmail.com>
2025-05-01 09:21:26 +0200

    Update footer to full name

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import "./Footer.css";
 
 function Footer() {
-  return <div className="Footer">© 2025, by Magali.</div>;
+  return <div className="Footer">© 2025, by Magali Boizot-Roche.</div>;
 }
 
 export default Footer;
