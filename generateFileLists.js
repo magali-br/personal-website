b997240853d65430d981852f616285134d0aa9de
Magali BR <magali.bzr@gmail.com>
2025-05-01 08:26:10 +0200

    Clean up unused code

--- generateFileLists.js
+++ generateFileLists.js
@@ -18,25 +18,4 @@ const generateFileListsSmart = async () => {
   }
 };
 
-// const generateFileLists = async () => {
-
-//   try {
-//     const files = await fs.readdir('./md/blog');
-//     const fileList = JSON.stringify(files, null, 2);
-//     await fs.writeFile('./src/blogFiles.json', fileList);
-//     console.log('Blog file list was generated successfully.');
-//   } catch (error) {
-//     console.error('Error generating blog file list:', error.message);
-//   }
-
-//   try {
-//     const files = await fs.readdir('./md/recipes');
-//     const fileList = JSON.stringify(files, null, 2);
-//     await fs.writeFile('./src/recipeFiles.json', fileList);
-//     console.log('Recipe file list was generated successfully.');
-//   } catch (error) {
-//     console.error('Error generating recipe file list:', error.message);
-//   }
-// };
-
 generateFileListsSmart();
