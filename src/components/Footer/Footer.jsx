282f6a27835dffd771aa29cb4a861cd5e8fea671
Magali BR <magali.bzr@gmail.com>
2026-04-09 13:12:24 +0200

    Add footer links to Home, LinkedIn, Instagram, Facebook with icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,55 @@
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   const currentYear = new Date().getFullYear();
-  return <div className="Footer">© {currentYear}, by Magali Boizot Roche.</div>;
+  return (
+    <div className="FooterBackground">
+      <span className="Footer">
+        <span className="FooterTextLink">
+          <Link to="/">{"Home"}</Link>
+          <a
+            className="Link"
+            href="https://www.instagram.com/offbeatcoaching/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="FooterIcon"
+              src={"/img/instagram_logo_white.png"}
+              alt={"Instagram account"}
+            />
+          </a>
+          <a
+            className="Link"
+            href="https://www.facebook.com/offbeatcoaching/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="FooterIcon"
+              src={"/img/facebook-24x24.png"}
+              alt={"Facebook account"}
+            />
+          </a>
+          <a
+            className="Link"
+            href="https://www.linkedin.com/in/magali-br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="FooterIcon"
+              src={"/img/linkedin-24x24.png"}
+              alt={"Instagram account"}
+            />
+          </a>
+        </span>
+
+        <span>© {currentYear}, Magali Boizot Roche.</span>
+      </span>
+    </div>
+  );
 }
 
 export default Footer;
