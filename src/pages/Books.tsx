21a9a45c37bf854e22ec0e078145bbad2efcb6a9
Magali BR <magali.bzr@gmail.com>
2026-05-07 16:26:16 +0200

    Fix hover on menu items in Navbar and Footer. Add option to change Navbar menu and title to center.

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -10,20 +10,43 @@ export const Books = () => {
 
   return (
     <div className="Container">
-      <div>
-        <p>
-          An evergrowing list of{" "}
-          <a
-            className="VisibleLink Link"
-            href={nonFictionBooksSheetShareUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            my favourite nonfiction books
-          </a>
-          :
-        </p>
-      </div>
+      <p>
+        I love to read, both fiction and nonfiction. As a child, I devoured
+        books. I have always loved stories, words, the ability each one of us
+        has to express something in our own unique way. When I became an adult,
+        I grew afraid that I was wasting my time reading fiction, and strived to
+        engage with the real world instead. This was a good goal, until it
+        became a I tried to live it out 24/7. Then came the burnouts.
+      </p>
+      <p>
+        I now understand that I actually <i>need</i> reading.
+      </p>
+      <p>
+        Nonfiction is my favourite way to engage with new ideas: it's
+        slow-paced, quiet, allows for reflection, creates space for me to engage
+        deeply with the topic. But fiction soothes me, calms me down, transports
+        me into lives I'll never have the chance to live. It also brings me
+        emotions, excitement, and novelty in a way that real life can't compete
+        with.
+      </p>
+      <p>
+        Fiction, and well-written nonfiction, teaches us about the world:
+        through storytelling, through other people's errors and explorations,
+        we, too, grow and gather experience we can use to choose how to interact
+        with the world.{" "}
+      </p>
+      <p>
+        Here is my evergrowing list of{" "}
+        <a
+          className="VisibleLink Link"
+          href={nonFictionBooksSheetShareUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          favourite nonfiction books
+        </a>
+        :
+      </p>
       <div className="w-full">
         <iframe
           src={nonFictionBooksSheetEmbedUrl}
@@ -37,20 +60,18 @@ export const Books = () => {
           Loading...
         </iframe>
       </div>
-      <div>
-        <p>
-          An evergrowing list of{" "}
-          <a
-            className="VisibleLink Link"
-            href={fictionBooksSheetShareUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            my favourite novels
-          </a>
-          :
-        </p>
-      </div>
+      <p>
+        And here's my terribly incomplete, evergrowing list of{" "}
+        <a
+          className="VisibleLink Link"
+          href={fictionBooksSheetShareUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          favourite works of fiction
+        </a>{" "}
+        (mostly novels) :
+      </p>
       <div className="w-full">
         <iframe
           src={fictionBooksSheetEmbedUrl}
